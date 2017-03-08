802eddf224b2b3b59d10d8461d2f74c8210a66b8
Burnsyy <cbyrn1@ocdsb.ca>
2017-03-08 12:41:49 -0500

    Update sketch.js

--- p5/Waving Snowman/sketch.js
+++ p5/Waving Snowman/sketch.js
@@ -1,7 +1,18 @@
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  ellipse(200, 200, 80, 80);
+  //ground
+rect(0,350,399,50);
+// bottom
+ellipse(200, 300, 150, 150);
+// middle
+ellipse(200, 200, 100, 100);
+//bottom
+ellipse(200, 120, 75, 75);
+//left arm
+line(160,200,50,100);
+//right arm
+line(240,200,350,122);
 }
