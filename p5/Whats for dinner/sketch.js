7586504881e5283c84433eba6d5aaf056a4eaa04
Burnsyy <cbyrn1@ocdsb.ca>
2017-03-10 12:51:02 -0500

    Update sketch.js

--- p5/Whats for dinner/sketch.js
+++ p5/Whats for dinner/sketch.js
@@ -1,7 +1,27 @@
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  ellipse(200, 200, 80, 80);
+  //table
+background(186, 145, 20); 
+// plate
+ellipse(200, 200, 350, 350); 
+ellipse(200, 200, 300, 300); 
+//fish
+noStroke();
+fill(48, 184, 184);
+bezier(133, 168, 386, 359, 427, 108, 111, 201);
+//fisheye
+fill(255, 255, 255);
+ellipse(302,208,10,10);
+//eyeball
+fill(0, 0, 0);
+ellipse(302,208,5,5);
+//potato
+fill(112, 59, 35);
+ellipse(200,115,133,75);
+//mash
+fill(161, 126, 126);
+ellipse(200,115, 110,14);
 }
