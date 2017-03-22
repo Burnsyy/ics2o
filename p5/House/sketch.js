b9b0cdc378abc4a5a2a98ac6f3a5e822f94398ac
Burnsyy <cbyrn1@ocdsb.ca>
2017-03-22 12:51:41 -0400

    Update sketch.js

--- p5/House/sketch.js
+++ p5/House/sketch.js
@@ -6,61 +6,72 @@ function setup() {
   myCanvas.position(200,200);
 }
 function draw() {
+var houseXPos =100;
+var houseYPos = houseXPos+100;
+var houseWidth = 200;
+var houseHeight = houseWidth/2;
+//sky
 noStroke();
-//background
-fill(130, 203, 237);
-rect(0, 0, 400, 400);
+background(63, 191, 250);
 //ground
-fill(214, 195, 195);
-rect(0,350,399,50);
-// bottom
-strokeWeight(3);
-stroke(0, 0, 0);
+fill(24, 240, 56);
+rect(-2,300,402,101);
+//clouds
 fill(255, 255, 255);
-ellipse(200, 300, 150, 150);
-// middle
-ellipse(200, 200, 100, 100);
-//bottom
-ellipse(200, 120, 75, 75);
-//left arm
-line(160,200,50,100);
-//right arm
-line(240,200,350,122);
-//snow
-strokeWeight(9);
-stroke(255, 255, 255);
-point(317,241);
-point(69,300);
-point(94,262);
-point(294,289);
-point(64,339);
-point(40,229);
-point(61,141);
-point(105,212);
-point(23,300);
-point(140,237);
-point(106,104);
-point(365,227);
-point(347,275);
-point(343,338);
-point(323,203);
-point(282,216);
-point(236,22);
-point(140,151);
-point(252,114);
-point(149,56);
-point(188,54);
-point(50,52);
-point(50,190);
-point(26,112);
-point(100,18);
-point(378,144);
-point(322,67);
-point(372,70);
-point(352,27);
-point(301,109);
-point(268,67);
-point(282,151);
-point(347,154);
-point(356,101);
+ellipse(100,100,30,30);
+ellipse(116,93,30,30);
+ellipse(100,78,30,30);
+ellipse(122,75,30,30);
+ellipse(142,93,30,30);
+ellipse(147,72,30,30);
+ellipse(131,100,30,30);
+ellipse(161,85,30,30);
+ellipse(94,91,30,30);
+
+ellipse(316,100-30,30,30);
+ellipse(316,93-30,30,30);
+ellipse(303,78-30,30,30);
+ellipse(323,75-30,30,30);
+ellipse(342,93-30,30,30);
+ellipse(338,72-30,30,30);
+ellipse(331,100-30,30,30);
+ellipse(350,85-30,30,30);
+ellipse(294,91-30,30,30);
+//sun
+fill(252, 213, 58);
+ellipse(0,0,100,100);
+//house
+stroke(0, 0, 0);
+fill(133, 64, 64);
+rect(houseXPos,houseYPos,houseWidth,houseHeight);
+//roof
+fill(41, 18, 18);
+triangle(houseXPos,houseYPos,houseXPos+houseWidth,houseYPos,houseXPos+houseWidth/2,houseYPos-houseHeight/2); 
+//door
+fill(117, 61, 61);
+rect(houseXPos+houseWidth/2,houseYPos+houseHeight/1.5,houseWidth/15,houseHeight/3);
+//windows
+fill(255, 255, 255);
+rect(houseXPos+houseWidth*1/4,houseYPos+houseHeight*1/4,houseWidth/8,houseHeight/4);
+rect(houseXPos+houseWidth*27/42,houseYPos+houseHeight*1/4,houseWidth/8,houseHeight/4);
+
+//2nd House
+houseXPos =150;
+houseYPos = 250;
+houseWidth = 100;
+houseHeight = houseWidth/2;
+//house
+stroke(0, 0, 0);
+fill(133, 64, 64);
+rect(houseXPos,houseYPos,houseWidth,houseHeight);
+//roof
+fill(41, 18, 18);
+triangle(houseXPos,houseYPos,houseXPos+houseWidth,houseYPos,houseXPos+houseWidth/2,houseYPos-houseHeight/2); 
+//door
+fill(117, 61, 61);
+rect(houseXPos+houseWidth/2,houseYPos+houseHeight/1.5,houseWidth/15,houseHeight/3);
+//windows
+fill(255, 255, 255);
+rect(houseXPos+houseWidth*1/4,houseYPos+houseHeight*1/4,houseWidth/8,houseHeight/4);
+rect(houseXPos+houseWidth*27/42,houseYPos+houseHeight*1/4,houseWidth/8,houseHeight/4);
 }
