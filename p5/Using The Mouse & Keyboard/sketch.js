52d9e4a360a738adb964e8480f0e76239c2a884c
Burnsyy <cbyrn1@ocdsb.ca>
2017-04-30 15:27:36 -0400

    Update sketch.js

--- p5/Using The Mouse & Keyboard/sketch.js
+++ p5/Using The Mouse & Keyboard/sketch.js
@@ -23,12 +23,11 @@ function draw() {
 	stroke (1);
 	colourSelection (); //draw colour display on top right
 	//code to crerate drawing
-	if (mouseIsPressed && mouseX >= 3 && mouseX <= 18 && mouseY >= 3 && mouseY <= 18) {
 	if (mouseIsPressed)  {
       noStroke ();
       fill (colourR, colourG, colourB);
-      ellipse (mouseX,mouseY,15,15);
-	}}
+      rect (mouseX,mouseY,15,15);
+	}
 }
 
 function shapeSelection () {
