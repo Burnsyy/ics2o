fac21745deaba793dc62782fe46c5154dbeff8b1
Burnsyy <cbyrn1@ocdsb.ca>
2017-05-15 13:13:06 -0400

    Add files via upload

---
+++ p5/Summative/sketch.js
@@ -0,0 +1,21 @@
+/* Summative Game
+Cory 
+Last Updated 2017-05-12
+*/
+
+var posX = ;
+var posY = ;
+var speedX = ;
+var speedY = ;
+
+
+function setup() {
+	var myCanvas = createCanvas(400, 400);
+	myCanvas.parent('Summative Game'); //create id for p5 sketch inorder to be able to insert it in html code
+	background(136, 188, 247); //create background 
+}
+function draw() {
+
+}	
+
+function ()
