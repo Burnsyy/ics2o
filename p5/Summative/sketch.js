4042da87c3b5c7087e0b7ee9e49f6dba2fd9de2a
Burnsyy <cbyrn1@ocdsb.ca>
2017-06-09 23:28:40 -0400

    Create sketch.js

--- p5/Summative/sketch.js
+++ p5/Summative/sketch.js
@@ -65,7 +65,7 @@ function menu () {
     text('HOW', 68, 337);
     text('SETTINGS',232, 337);
     
-    /* if (mouseIsPressed && mouseX >= 80 && mouseX <= 320 && mouseY >= 175 && mouseY <= 225) {
+     if (mouseIsPressed && mouseX >= 80 && mouseX <= 320 && mouseY >= 175 && mouseY <= 225) {
         screen = 2;
     }
     if (mouseIsPressed && mouseX >= 30 && mouseX <= 170 && mouseY >= 300 && mouseY <= 350) {
@@ -73,7 +73,7 @@ function menu () {
     }
     if (mouseIsPressed && mouseX >= 230 && mouseX <= 370 && mouseY >= 300 && mouseY <= 350) {
         screen = 4;
-    } */
+    } 
 };
 } //Menu
 
